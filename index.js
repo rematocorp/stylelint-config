9a0b5d61e1c26069cf2c117a85509ec25da70c0e
Uku Pattak <uku.pattak@remato.ee>
2021-01-09 22:30:01 +0200

    Add initial config

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: ["stylelint-declaration-use-variable"],
+  rules: {
+    "at-rule-disallowed-list": ["keyframes"],
+    "block-no-empty": true,
+    "sh-waqar/declaration-use-variable": [
+      [
+        "/color/",
+        {
+          ignoreValues: [
+            "transparent",
+            "inherit",
+            "currentColor",
+            "initial",
+            "/darken/",
+            "/lighten/",
+            "/rgba/",
+          ],
+        },
+      ],
+    ],
+  },
+};
