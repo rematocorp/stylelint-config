55794d852121e28c90b23ff007bcaa5b138efd50
Uku Pattak <uku.pattak@remato.ee>
2021-01-17 19:43:27 +0200

    Release 1.0.3
    Add more color validations

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ module.exports = {
 	rules: {
 		'at-rule-disallowed-list': ['keyframes'],
 		'block-no-empty': true,
+		'color-no-invalid-hex': true,
+		'color-named': 'never',
 		'sh-waqar/declaration-use-variable': [
 			[
 				'/color/',
