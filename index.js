55e23a12f793eb0dd3530d0a269e0d2b13da63b3
Uku Pattak <uku.pattak@remato.ee>
2021-02-14 20:36:14 +0200

    Avoid errors

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 module.exports = {
 	plugins: ['stylelint-declaration-use-variable'],
 	rules: {
-		'at-rule-disallowed-list': ['keyframes'],
-		'block-no-empty': true,
+		// Avoids errors
 		'color-no-invalid-hex': true,
+		'block-no-empty': true,
+		'function-calc-no-invalid': true,
+		'function-calc-no-unspaced-operator': true,
+		'unit-no-unknown': true,
+		'property-no-unknown': true,
+		'keyframe-declaration-no-important': true,
+		'declaration-block-no-duplicate-properties': [
+			true,
+			{
+				ignore: ['consecutive-duplicates-with-different-values'],
+			},
+		],
+		'declaration-block-no-shorthand-property-overrides': true,
+		'selector-pseudo-class-no-unknown': true,
+		'selector-pseudo-element-no-unknown': true,
+		'selector-type-no-unknown': true,
+		'media-feature-name-no-unknown': true,
+		'at-rule-no-unknown': [
+			true,
+			{
+				ignoreAtRules: ['mixin', 'include'],
+			},
+		],
+		'no-duplicate-selectors': true,
+		'no-duplicate-at-import-rules': true,
+		'no-empty-source': true,
+		'no-extra-semicolons': true,
+
+		// Enforces DRY
+		'at-rule-disallowed-list': ['keyframes'],
 		'color-named': 'never',
 		'sh-waqar/declaration-use-variable': [
 			[
