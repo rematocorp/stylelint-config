12cd6400e13dbe2045c903cae010b97539e6b073
Uku Pattak <uku.pattak@remato.ee>
2021-04-10 16:46:39 +0300

    Add no-prefix rules

--- index.js
+++ index.js
@@ -43,5 +43,12 @@ module.exports = {
 				},
 			],
 		],
+
+		// Avoids doing autoprefixer job
+		'value-no-vendor-prefix': true,
+		'property-no-vendor-prefix': true,
+		'selector-no-vendor-prefix': true,
+		'media-feature-name-no-vendor-prefix': true,
+		'at-rule-no-vendor-prefix': true,
 	},
 }
